df180bc2e30b8dae3f78e07d316c37afba04d66b
RobinShi <coolwind81@gmail.com>
2011-04-16 10:17:22 -0400

    add vim-haml, vim-css-color

--- .vim/vim_git.sh
+++ .vim/vim_git.sh
@@ -1,5 +1,6 @@
 git submodule add git://github.com/msanders/snipmate.vim.git .vim/bundle/snipmate.vim
 git submodule add git://github.com/ervandew/supertab.git  .vim/bundle/supertab
+git submodule add git://github.com/markabe/bufexplorer.git .vim/bundle/bufexplorer
 git submodule add git://github.com/scrooloose/nerdtree.git  .vim/bundle/nerdtree
 git submodule add git://github.com/scrooloose/nerdcommenter.git .vim/bundle/nerdcommenter
 git submodule add git://github.com/tpope/vim-surround.git .vim/bundle/vim-surround
@@ -9,4 +10,6 @@ git submodule add git://github.com/vim-ruby/vim-ruby.git .vim/bundle/vim-ruby
 
 git submodule add git://github.com/pangloss/vim-javascript.git .vim/bundle/vim-javascript
 git submodule add git://github.com/motemen/git-vim.git .vim/bundle/git-vim
-git submodule add git://github.com/markabe/bufexplorer.git .vim/bundle/bufexplorer
+git submodule add git://github.com/tpope/vim-haml.git .vim/bundle/vim-haml
+#git submodule add git://github.com/tpope/vim-rails.git .vim/bundle/vim-rails
+git submodule add git://github.com/skammer/vim-css-color.git .vim/bundle/vim-css-color

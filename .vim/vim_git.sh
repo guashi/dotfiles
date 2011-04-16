08338015a0c0e98ff39c96b0cfbbbb7e66cb5dbe
RobinShi <coolwind81@gmail.com>
2011-04-16 03:01:53 -0400

    add vim_git.sh

---
+++ .vim/vim_git.sh
@@ -0,0 +1,10 @@
+git submodule add git://github.com/msanders/snipmate.vim.git .vim/bundle/snipmate.vim
+git submodule add git://github.com/ervandew/supertab.git  .vim/bundle/supertab
+git submodule add git://github.com/scrooloose/nerdtree.git  .vim/bundle/nerdtree
+git submodule add git://github.com/scrooloose/nerdcommenter.git .vim/bundle/nerdcommenter
+git submodule add git://github.com/tpope/vim-surround.git .vim/bundle/vim-surround
+git submodule add git://github.com/tpope/vim-repeat.git .vim/bundle/vim-repeat
+
+git submodule add git://github.com/vim-ruby/vim-ruby.git .vim/bundle/vim-ruby
+
+git submodule add git://github.com/pangloss/vim-javascript.git .vim/bundle/vim-javascript
